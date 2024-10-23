be6c3be50f8e6c7609851fbc6358d7f8c557df46
Jessé <jessepescador22@gmail.com>
2024-10-23 19:48:00 -0300

    adcionando biblioteca

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div className="container">
       <form>
         <h1>Acesse o sistema</h1>
-        <input type="email" placeholder='E-mail' />
+        <div>
+          <input type="email" placeholder="E-mail" />
+          <FaUser className="icon"/>
+        </div>
+
+        <div>
+          <input type="password" placeholder="Senha" />
+          <FaLock/>
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+        <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>
+          </p>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

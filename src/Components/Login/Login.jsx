8bbb74b62ed3922daf2a12e3caede38d9779b165
Jessé <jessepescador22@gmail.com>
2024-10-23 20:14:34 -0300

    finalizando

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
+import "./Login.css";
 
 const Login = () => {
+  // Estados para armazenar as entradas do usuário
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Função que é chamada quando o formulário é enviado
+  const handleSubmit = (event) => {
+    // Impede que a página seja recarregada
+    event.preventDefault();
+
+    // Faz o console log das credenciais do usuário
+    console.log("Dados de Login:", { username, password });
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
-        <div>
-          <input type="email" placeholder="E-mail" />
-          <FaUser className="icon"/>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
         </div>
-
-        <div>
-          <input type="password" placeholder="Senha" />
-          <FaLock/>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
         </div>
 
         <div className="recall-forget">
@@ -20,13 +46,12 @@ const Login = () => {
             <input type="checkbox" />
             Lembre de mim
           </label>
-          <a href="#">Esqueceu a senha?</a>
+          <a href="#">Esqueceu sua senha?</a>
         </div>
-        <button>Entrar</button>
-
+        <button type="submit">Login</button>
         <div className="signup-link">
           <p>
-            Não tem uma conta? <a href="#">Registrar</a>
+            Não tem uma conta? <a href="#">Registar</a>{" "}
           </p>
         </div>
       </form>
